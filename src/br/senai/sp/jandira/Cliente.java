ae1b2b5f328a9b6f70ef24cad2a45d0b63a91293
Celso Furtado <celso.furtado@sp.senai.br>
2022-08-31 16:09:01 -0300

    Enviando de novo...

--- src/br/senai/sp/jandira/Cliente.java
+++ src/br/senai/sp/jandira/Cliente.java
@@ -3,8 +3,10 @@ package br.senai.sp.jandira;
 public class Cliente {
 	
 	public String nome;
-	public String email;
 	public String cidade;
 	public int idade;
+	public int teste;
+	public int teste2;
+	public int teste3;
 
 }
